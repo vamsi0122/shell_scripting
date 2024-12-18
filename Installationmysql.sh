9b933e42f6df2526449b0f3cb88c2da563b4f4d8
vamsi0122 <suryavamsi766@gmail.com>
2024-12-18 11:57:23 +0530

    shell

--- Installationmysql.sh
+++ Installationmysql.sh
@@ -1,7 +1,29 @@
+DATE=$(date +%F)
+LOGFILE=/tmp/$0-$DATE
+RED="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+#functions
+Validation () {
+    if [$1 -e 0]
+    then
+       echo -e $2 is $G sucess $N
+    else
+       echo -e $2 is $R failure $-n
+    fi
+    
+}
+
 USERID=$(id -u)
 if [USERID -ne 0]
 then
     echo"No root access to install switch to root"
     exit 1 #terminate the execution here.
 fi
-yum install mysql -y
+
+yum install mysql -y &>>$LOGFILE
+Validation $? "Installing mysql"
+
+yum install postfix -y &>>$LOGFILE
+Validation $? "Installing postfix"
